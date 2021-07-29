c742832f8965d1d81c9be749bde1a6c629891857
Arnold Burnley <aburnleylvx@outlook.com>
2021-07-29 08:09:54 -0400

    implemented cors

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'The Dating App';
+export class AppComponent implements OnInit {
+ title = 'The Dating App';
+ URL = 'https://localhost:5001/api/users';
+ users: any;
+  
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get(this.URL).subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    })
+  }
+
 }
